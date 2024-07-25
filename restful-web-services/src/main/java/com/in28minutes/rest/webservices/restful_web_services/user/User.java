de535a395729274b228a35be6dab5995dfa217af
garyliang <liang.ji.wei1@gmail.com>
2024-07-25 15:59:38 +0800

    update

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/user/User.java
@@ -2,9 +2,16 @@ package com.in28minutes.rest.webservices.restful_web_services.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
     private Integer id;
+
+    @Size(min = 2, message = "Name should have at least 2 characters")
     private String name;
+
+    @Past(message = "Birth day should be in the past")
     private LocalDate birthDate;
     
     public User(int id, String name, LocalDate birthDate) {
