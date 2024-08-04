43cd81ef3c78215b63ee4c1817706dd8739eb175
DerickLiang <liang.ji.wei1@gmail.com>
2024-08-04 22:03:31 +0800

    update

--- springboot-exersice-project/src/main/java/com/springboot/exercise/springboot_exersice_project/controller/CommentController.java
+++ springboot-exersice-project/src/main/java/com/springboot/exercise/springboot_exersice_project/controller/CommentController.java
@@ -1,26 +1,17 @@
 package com.springboot.exercise.springboot_exersice_project.controller;
 
-import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.exercise.springboot_exersice_project.dto.CommentGetDto;
 import com.springboot.exercise.springboot_exersice_project.dto.CommentPostDto;
-import com.springboot.exercise.springboot_exersice_project.entity.Comment;
-import com.springboot.exercise.springboot_exersice_project.entity.Post;
-import com.springboot.exercise.springboot_exersice_project.repository.CommentRepository;
-import com.springboot.exercise.springboot_exersice_project.repository.PostRepository;
-import com.springboot.exercise.springboot_exersice_project.repository.UserDetailsRepository;
 import com.springboot.exercise.springboot_exersice_project.service.CommentServiceImp;
 
 
