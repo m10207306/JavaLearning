2d616f8b24ff98bc5a43a56b657c505d5b5863bf
garyliang <liang.ji.wei1@gmail.com>
2024-08-08 15:09:20 +0800

    delete useless code

--- springboot-exersice-project/src/main/java/com/springboot/exercise/springboot_exersice_project/dto/ConfigBean.java
+++ springboot-exersice-project/src/main/java/com/springboot/exercise/springboot_exersice_project/dto/ConfigBean.java
@@ -1,6 +1,5 @@
 package com.springboot.exercise.springboot_exersice_project.dto;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
